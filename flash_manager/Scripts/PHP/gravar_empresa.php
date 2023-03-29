6be0a0aa171436e2d951944effe28dd5c7cbebae
eduardorodrigues1995 <64078473+eduardorodrigues1995@users.noreply.github.com>
2023-03-29 09:05:35 -0300

    Add files via upload

--- flash_manager/Scripts/PHP/gravar_empresa.php
+++ flash_manager/Scripts/PHP/gravar_empresa.php
@@ -7,13 +7,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obter os dados da empresa do formulário
     $cliente_empresa_cnpj = $_POST["cliente_empresa_cnpj"];
     $cliente_empresa_razao_social = $_POST["cliente_empresa_razao_social"];
-    $situaçãocnpj = $_POST["situaçãocnpj"];
+    $situacao_cnpj = $_POST["situacao_cnpj"];
     $cliente_empresa_data_abertura = $_POST["cliente_empresa_data_abertura"];
     $cliente_empresa_natureza_juridica = $_POST["cliente_empresa_natureza_juridica"];
     $cliente_empresa_capital_social = $_POST["cliente_empresa_capital_social"];
     $cliente_empresa_telefone = $_POST["cliente_empresa_telefone"];
     $cliente_empresa_email = $_POST["cliente_empresa_email"];
-    $cliente_empresa_Instagram = $_POST["cliente_empresa_Instagram"];
+    $cliente_empresa_instagram = $_POST["cliente_empresa_instagram"];
     $cliente_empresa_rua = $_POST["cliente_empresa_rua"];
     $cliente_empresa_n_complemento = $_POST["cliente_empresa_n_complemento"];
     $cliente_empresa_bairro = $_POST["cliente_empresa_bairro"];
@@ -22,11 +22,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $cliente_empresa_cep = $_POST["cliente_empresa_cep"];
 
     // Preparar a consulta SQL
-    $stmt = $pdo->prepare("INSERT INTO pessoa_juridica (cnpj, razao_social, situacao_cnpj, data_abertura, natureza_juridica, capital_social, telefone, email, instagram, rua, n_complemento, bairro, uf, cidade, cep) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO pessoa_juridica (cnpj, razao_social, situacao_cnpj, data_abertura, natureza_juridica, capital_social, telefone, email, instagram, rua, n_complemento, bairro, uf, cidade, cep) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
     if ($stmt) {
         // Vincular os parâmetros da consulta
-        $stmt->bind_param("sssssssssssssss", $cliente_empresa_cnpj, $cliente_empresa_razao_social, $situaçãocnpj, $cliente_empresa_data_abertura, $cliente_empresa_natureza_juridica, $cliente_empresa_capital_social, $cliente_empresa_telefone, $cliente_empresa_email, $cliente_empresa_Instagram, $cliente_empresa_rua, $cliente_empresa_n_complemento, $cliente_empresa_bairro, $cliente_empresa_uf, $cliente_empresa_cidade, $cliente_empresa_cep);
+        $stmt->bind_param("sssssssssssssss", $cliente_empresa_cnpj, $cliente_empresa_razao_social, $situacao_cnpj, $cliente_empresa_data_abertura, $cliente_empresa_natureza_juridica, $cliente_empresa_capital_social, $cliente_empresa_telefone, $cliente_empresa_email, $cliente_empresa_instagram, $cliente_empresa_rua, $cliente_empresa_n_complemento, $cliente_empresa_bairro, $cliente_empresa_uf, $cliente_empresa_cidade, $cliente_empresa_cep);
 
         // Executar a consulta SQL
         if ($stmt->execute()) {
@@ -41,3 +41,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Fechar a declaração
         $stmt->close();
+    } else {
+        // Log da falha na operação
+        error_log('Erro ao preparar a consulta SQL: ' . $conn->error);
+        echo json_encode(array("message" => "Erro ao preparar a consulta SQL"));
+    }
+} else {
+    // Log da falha na operação
+    error_log('Requisição inválida');
+    echo json_encode(array("message" => "Requisição inválida"));
+}
