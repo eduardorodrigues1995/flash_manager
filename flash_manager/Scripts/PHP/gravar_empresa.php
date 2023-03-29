093b2b681e0e9bd505d3cd72bea134681b80aa93
eduardorodrigues1995 <64078473+eduardorodrigues1995@users.noreply.github.com>
2023-03-29 08:12:29 -0300

    Update gravar_empresa.php

--- flash_manager/Scripts/PHP/gravar_empresa.php
+++ flash_manager/Scripts/PHP/gravar_empresa.php
@@ -1,71 +1,30 @@
 <?php
-// Configuração do log
-ini_set('error_log', '/var/log/meu_programa.log');
-ini_set('log_errors', 'On');
+// Inclui o arquivo de conexão com o banco de dados
+include_once 'conexao_db.php';
 
-// Inclua o arquivo de conexão com o banco de dados
-include('conexao_db.php');
+// Prepara a consulta SQL
+$stmt = $conn->prepare("INSERT INTO pessoa_juridica (cnpj, razao_social, situacao_cnpj, data_abertura, natureza_juridica, capital_social, telefone, email, instagram, rua, n_complemento, bairro, uf, cidade, cep) 
+VALUES (:cnpj, :razao_social, :situacao_cnpj, :data_abertura, :natureza_juridica, :capital_social, :telefone, :email, :instagram, :rua, :n_complemento, :bairro, :uf, :cidade, :cep)");
 
-// Inicialize a variável de mensagem
-$msg = '';
+// Vincula os parâmetros
+$stmt->bindParam(':cnpj', $_POST['cliente_empresa_cnpj']);
+$stmt->bindParam(':razao_social', $_POST['cliente_empresa_razao_social']);
+$stmt->bindParam(':situacao_cnpj', $_POST['situacao_cnpj']);
+$stmt->bindParam(':data_abertura', $_POST['cliente_empresa_data_abertura']);
+$stmt->bindParam(':natureza_juridica', $_POST['cliente_empresa_natureza_juridica']);
+$stmt->bindParam(':capital_social', $_POST['cliente_empresa_capital_social']);
+$stmt->bindParam(':telefone', $_POST['cliente_empresa_telefone']);
+$stmt->bindParam(':email', $_POST['cliente_empresa_email']);
+$stmt->bindParam(':instagram', $_POST['cliente_empresa_Instagram']);
+$stmt->bindParam(':rua', $_POST['cliente_empresa_rua']);
+$stmt->bindParam(':n_complemento', $_POST['cliente_empresa_n_complemento']);
+$stmt->bindParam(':bairro', $_POST['cliente_empresa_bairro']);
+$stmt->bindParam(':uf', $_POST['cliente_empresa_uf']);
+$stmt->bindParam(':cidade', $_POST['cliente_empresa_cidade']);
+$stmt->bindParam(':cep', $_POST['cliente_empresa_cep']);
 
-// Verifique se o formulário foi enviado
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Capturar os valores dos campos do formulário
-  $cnpj = $_POST['cliente_empresa_cnpj'];
-  $razao_social = $_POST['cliente_empresa_razao_social'];
-  $situacao_cnpj = $_POST['situaçãocnpj'];
-  $data_abertura = $_POST['cliente_empresa_data_abertura'];
-  $natureza_juridica = $_POST['cliente_empresa_natureza_juridica'];
-  $capital_social = $_POST['cliente_empresa_capital_social'];
-  $telefone = $_POST['cliente_empresa_telefone'];
-  $email = $_POST['cliente_empresa_email'];
-  $instagram = $_POST['cliente_empresa_Instagram'];
-  $rua = $_POST['cliente_empresa_rua'];
-  $n_complemento = $_POST['cliente_empresa_n_complemento'];
-  $bairro = $_POST['cliente_empresa_bairro'];
-  $uf = $_POST['cliente_empresa_uf'];
-  $cidade = $_POST['cliente_empresa_cidade'];
-  $cep = $_POST['cliente_empresa_cep'];
+// Executa a consulta
+$stmt->execute();
 
-  // Preparar a consulta SQL para inserir os valores na tabela pessoa_juridica
-  $stmt = $pdo->prepare("
-    INSERT INTO pessoa_juridica 
-      (cnpj, razao_social, situacao_cnpj, data_abertura, natureza_juridica, capital_social, telefone, email, instagram, rua, n_complemento, bairro, uf, cidade, cep)
-    VALUES 
-      (:cnpj, :razao_social, :situacao_cnpj, :data_abertura, :natureza_juridica, :capital_social, :telefone, :email, :instagram, :rua, :n_complemento, :bairro, :uf, :cidade, :cep)
-  ");
-
-  // Vincular os valores dos parâmetros com os valores dos campos do formulário
-  $stmt->bindParam(':cnpj', $cnpj);
-  $stmt->bindParam(':razao_social', $razao_social);
-  $stmt->bindParam(':situacao_cnpj', $situacao_cnpj);
-  $stmt->bindParam(':data_abertura', $data_abertura);
-  $stmt->bindParam(':natureza_juridica', $natureza_juridica);
-  $stmt->bindParam(':capital_social', $capital_social);
-  $stmt->bindParam(':telefone', $telefone);
-  $stmt->bindParam(':email', $email);
-  $stmt->bindParam(':instagram', $instagram);
-  $stmt->bindParam(':rua', $rua);
-  $stmt->bindParam(':n_complemento', $n_complemento);
-  $stmt->bindParam(':bairro', $bairro);
-  $stmt->bindParam(':uf', $uf);
-  $stmt->bindParam(':cidade', $cidade);
-  $stmt->bindParam(':cep', $cep);
-
-  // Executar a consulta SQL
-$registroInserido = false;
-if ($stmt->execute()) {
-  // Atribuir mensagem de sucesso à variável
-  $msg = 'Cadastro realizado com sucesso!';
-
-  // Redirecionar o usuário para outra página
-  header('Location: /pagina-de-sucesso.php');
-  exit;
-} else {
-  // Atribuir mensagem de erro à variável
-  $msg = 'Erro ao realizar o cadastro.';
-  // Registrar mensagem de log
-  error_log("Erro ao inserir registro na tabela pessoa_juridica: " . $stmt->errorInfo()[2]);
-}
-}
+echo "Registro inserido com sucesso!";
+?>
